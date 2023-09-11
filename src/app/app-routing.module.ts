5a2ea139f72168b97b1f27528fa296d312402439
Austin <17032781@mstc.edu>
2023-09-11 15:05:49 -0500

    Completed project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }, // The default route redirects to /dashboard
+  { path: 'dashboard', component: DashboardComponent }, // The dashboard route
+  { path: 'detail/:id', component: HeroDetailComponent }, // The colon (:) in the path indicates that :id is a placeholder for a specific hero id
+  { path: 'heroes', component: HeroesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
